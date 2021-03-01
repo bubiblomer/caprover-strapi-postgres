0598778935e0a87c2c4091268a92d799e98a7e46
bubiblomer <bubiblomer@gmail.com>
2021-03-01 00:05:17 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '399fa4de700dc2fddb634c8131e902ac'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e35b751a-6ce7-40a2-8dc9-5a89e4af4372'
+};
